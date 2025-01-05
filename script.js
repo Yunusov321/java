d8e441a21d30eac6b40bac5077c5603e2568e079
Yunusov321 <diloromsaidova93@gmail.com>
2025-01-05 14:19:57 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let fruits = ["apple", "banana", "orange"];
+
+for (let element of fruits) {
+    console.log(element);
+}
+
+fruits.sort();
+console.log(fruits);
+
+let numbers = [1,2,3,4,6,0]
+numbers.reverse();
+console.log(numbers);
